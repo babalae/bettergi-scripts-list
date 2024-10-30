caff453bfd8da794bb56cee5d41e7b2b8583b7f5
Lix <iclae0@gmail.com>
2024-10-31 01:07:17 +0800

    优化传送到合成台和传送到凯瑟琳

--- repo/js/TheTable/main.js
+++ repo/js/TheTable/main.js
@@ -1,17 +1,7 @@
 (async function () {
     setGameMetrics(1920, 1080, 2);
-    keyPress("M");
-    await sleep(1500);
-    click(1842, 1018);
-    await sleep(1500);
-    click(1467, 358);
-    await sleep(1500);
-    click(959, 543);
-    await sleep(1500);
-    click(1300, 735);
-    await sleep(1500);
-    click(1570, 1009);
-    await sleep(10000);
+    // 传送到枫丹冒险家公会附近传送点
+    await genshin.tp(4514.18, 3630.4);
     keyDown("w");
     await sleep(4500);
     keyUp("w");
