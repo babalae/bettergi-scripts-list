0c1ee03f32d3d11d5f2b526ef31ceeb5d30eee68
宇寒 <149793500+YuHan1015@users.noreply.github.com>
2024-12-31 20:16:19 +0800

    Add files via upload

---
+++ repo/js/Who-is-true-Nailong/main.js
@@ -0,0 +1,23 @@
+(async function () {
+    while (1){
+        log.info('我是奶龙');
+        await sleep(2000);
+        log.info('我才是奶龙');
+        await sleep(2000);
+        log.info('我才是奶龙！');
+        await sleep(2000);
+        log.info('今夜星光闪闪');
+        await sleep(2000);
+        log.info('我爱你的心满满');
+        await sleep(2000);
+        log.info('想你一晚又一晚');
+        await sleep(2000);
+        log.info('把爱你的心都填满');
+        await sleep(2000);
+        log.info('想受爱情的苦');
+        await sleep(2000);
+        log.info('做你的小公主');
+        await sleep(2000);
+        await sleep(2000);
+    }
+})();
