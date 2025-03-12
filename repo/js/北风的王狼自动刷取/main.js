a542cba46a1d9d12c17eeda913a703069e1ad4ee
5117600049 <511760049@qq.com>
2025-03-12 19:53:22 +0800

    Update main.js

--- repo/js/北风的王狼自动刷取/main.js
+++ repo/js/北风的王狼自动刷取/main.js
@@ -1,10 +1,13 @@
 (async function () {
-     await pathingScript.runFile("assets/前往狼王.json");
-    await sleep(1000);
-    keyPress("F");
-    await sleep(13000);
+await pathingScript.runFile("assets/前往狼王.json");
+await sleep(1000);
+keyPress("F");
+await sleep(13000);
 await dispatcher.runTask(new SoloTask("AutoFight"));
- await sleep(1000);
- await pathingScript.runFile("assets/领取奖励.json");
-    keyPress("F");
+await sleep(1000);
+await pathingScript.runFile("assets/领取奖励.json");
+keyPress("F");
+await sleep(5000);
+click(975, 1000);//消耗树脂领取
+await sleep(1000);
 })();
