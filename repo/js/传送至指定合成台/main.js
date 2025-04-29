5abf340dfe3a3037b7975b0491b2e2a7fcc64933
NaraLan0502 <129516870+NaraLan0502@users.noreply.github.com>
2025-04-30 00:04:08 +0800

    添加“传送至指定合成台”js脚本 (#672)
    * Add files via upload
    
    * 传送至指定合成台
    
    * Delete repo/js/蒙德合成台.json
    
    * Delete repo/js/main.js
    
    * Delete repo/js/manifest.json
    
    * Delete repo/js/settings.json
    
    * Delete repo/js/枫丹合成台.json
    
    * Delete repo/js/璃月合成台.json
    
    * Delete repo/js/稻妻合成台.json
    
    * Update settings.json

---
+++ repo/js/传送至指定合成台/main.js
@@ -0,0 +1,28 @@
+async function AutoPath(locationName) {
+  try {
+  let filePath = `assets/${locationName}.json`;
+      await pathingScript.runFile(filePath);
+      sleep(2000);
+
+      return true;
+  } catch (error) {
+      log.error(`执行 ${locationName} 路径时发生错误`);
+      log.error(error.message);
+  }
+  
+  return false;
+}
+(
+  async function () {
+    try {
+      await genshin.returnMainUi();
+      setGameMetrics(1920, 1080, 1)
+      // 读取配置文件
+      let location = settings.location;
+      await AutoPath(location);
+    } catch (e) {
+      log.error("传送失败，请检查设置");
+      return;
+      }
+  }
+)();
