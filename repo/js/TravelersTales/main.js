37dcece5f64baca3133e4e498498e2d81795b183
ddaodan <40017293+ddaodan@users.noreply.github.com>
2025-06-22 01:52:32 +0800

    0.0.4 - **修复无法对话的bug** - 优化部分日志信息的显示 - 新增对话检测，进入对话失败则不会进行记录 - 优化连续运行的逻辑 - 调整部分识别点的地图缩放 - 补充路线

--- repo/js/TravelersTales/main.js
+++ repo/js/TravelersTales/main.js
@@ -7,38 +7,46 @@
     // 前往七天神像
     await genshin.tpToStatueOfTheSeven();
     let runCount = 0;
-    const maxRuns = 5;
+    const maxRuns = 50;
     while (runCount < maxRuns) {
-        log.info(`第${runCount + 1}次运行，当前进度：${runCount}/${maxRuns}`);
+        log.info(`开始第${runCount + 1}次运行`);
         await find();
+        if (!foundAvatar) {
+            log.info("未找到角色，结束运行");
+            return;
+        }
         log.info(`找到角色头像，正在查找对应角色...`);
         const characterPositions = [
             { name: "柯莱-1", x: 2843, y: -384, hasKeyMouse: true },
             { name: "迪希雅-1", x: 3771, y: 3608 },
+            { name: "迪希雅-2", x: 4436, y: 3539 },
             { name: "赛诺-1", x: 3062, y: -268 },
+            { name: "赛诺-2", x: 3059, y: -268 },
+            { name: "赛诺-3", x: 2863, y: -380 },
             { name: "林尼-1", x: 4197, y: 4805 },
             { name: "夏沃蕾-1", x: 4356, y: 3707 },
             { name: "菲米尼-1", x: 4202, y: 3037, hasKeyMouse: true },
-            { name: "夏洛蒂-1", x: 4618, y: 3518},
+            { name: "夏洛蒂-1", x: 4618, y: 3518 },
+            { name: "夏洛蒂-2", x: 4642, y: 3495 },
             { name: "绮良良-1", x: 231, y: -672 },
             { name: "绮良良-2", x: 231, y: -672 },
+            { name: "绮良良-3", x: -4473, y: -2655 },
             { name: "鹿野院平藏-1", x: -4459, y: -3141 },
             { name: "鹿野院平藏-2", x: -4467, y: -3127 },
-            { name: "托马-1", x: -4399, y: -3130},
+            { name: "鹿野院平藏-3", x: -4417, y: -3037 },
+            { name: "托马-1", x: -4399, y: -3130 },
             { name: "梦见月瑞希-1", x: -4458, y: -3111, hasKeyMouse: true },
             { name: "梦见月瑞希-2", x: -4458, y: -3111, hasKeyMouse: true },
             { name: "八重神子-1", x: -4424, y: -2475 },
             { name: "那维莱特-1", x: 3600, y: 3804 },
+            { name: "那维莱特-2", x: 4472, y: 3553 },
             { name: "神里绫人-1", x: -4473, y: -3132 }
         ];
 
         hasKeyMouse = false;
         let found = false;
         for (const pos of characterPositions) {
-            if (config[pos.name] === true) {
-                continue;
-            }
-            if (isNearPosition(characterX, characterY, pos.x, pos.y)) {
+            if (!config[pos.name] && isNearPosition(characterX, characterY, pos.x, pos.y)) {
                 pathingName = pos.name;
                 hasKeyMouse = !!pos.hasKeyMouse;
                 found = true;
@@ -53,8 +61,8 @@
         }
         log.info(`正在前往 ${pathingName} 所在位置...`);
         await pathingScript.runFile(`assets/pathing/${pathingName}.json`)
+        keyPress("F");
         if (!hasKeyMouse) {
-            keyPress("F");
             log.info("开始对话...");
         }
         await sleep(3000);
@@ -68,14 +76,18 @@
             await waitToMain();
         }
         config[pathingName] = true;
-        await file.writeText("config.json", JSON.stringify(config, null, 4));
-        log.info(`任务完成，已更新配置文件`);
+        if (failed) {
+            log.info("本次运行结果不会被保存");
+        } else {
+            await file.writeText("config.json", JSON.stringify(config, null, 4));
+            log.info(`对话完成，已保存本次运行结果`);
+        }
         runCount++;
         if (runCount < maxRuns) {
             log.info(`第${runCount}次运行完成`);
         }
     }
-    log.info(`已对话${maxRuns}次，今日对话次数已达到上限。`);
+    log.info(`已对话${maxRuns}次，结束运行。`);
 })();
 
 /**
@@ -104,11 +116,11 @@ async function find() {
         log.info(`第 ${retryCount + 1} 次尝试定位...`);
         log.info(`移动到位置：(${position.x}, ${position.y}), ${position.name || '未命名位置'}`);
         await genshin.moveMapTo(position.x, position.y, position.country);
-
-        const found = await locate();
-        if (found) return;
+        foundAvatar = await locate();
+        if (foundAvatar) return;
     }
-    throw new Error("寻找失败，已达最大重试次数");
+    log.error("寻找所有角色可能存在的位置都没有找到角色");
+    return;
 }
 
 async function locate() {
@@ -144,7 +156,9 @@ async function waitToMain() {
     const paimonMenuRo = RecognitionObject.TemplateMatch(file.ReadImageMatSync("assets/icon/paimon_menu.png"), 0, 0, genshin.width / 3.0, genshin.width / 5.0);
     const maxRetries = 180; // 设置最大重试次数以防止无限循环
     let retries = 0;
+    let enteredLoop = false;
     while (captureGameRegion().Find(paimonMenuRo).isEmpty()) {
+        enteredLoop = true;
         if (retries >= maxRetries) {
             log.error("返回主界面超时");
             return false;
@@ -152,7 +166,12 @@ async function waitToMain() {
         await sleep(1000);
         retries++;
     }
-    return;
+    if (!enteredLoop || !hasKeyMouse) {
+        log.error("进入对话失败");
+        failed = true;
+        return false;
+    }
+    return true;
 }
 
 /**
