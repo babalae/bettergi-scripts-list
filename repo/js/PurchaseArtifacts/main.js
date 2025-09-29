5a6142ff17de2e3e0e5fa7b8659c960b3e0bfe83
skyflag2022 <107539971+skyflag2022@users.noreply.github.com>
2025-09-29 21:46:47 +0800

    修改地图追踪路线 (#2030)
    * 增加挪德卡莱点位
    
    改了版本号，增加了点位
    
    * 移除无用字段
    
    * Update manifest.json
    
    * 修改了地图追踪任务，更改了时间调节方式
    
    优化线路
    
    ---------
    
    Co-authored-by: 起个名字好难的喵 <25520958+MisakaAldrich@users.noreply.github.com>

--- repo/js/PurchaseArtifacts/main.js
+++ repo/js/PurchaseArtifacts/main.js
@@ -25,20 +25,6 @@ let holdingState = settings.holdingState ?? 0;
     await Purchase('蒙德购买狗粮');
     await Shopping();
 
-//调整时间
-    await sleep(1500);
-    keyPress("ESCAPE"); await sleep(1500);
-    click(45, 715); await sleep(1500);
-    moveMouseTo(1440,510 );
-    leftButtonDown(); await sleep(1500);
-    moveMouseTo(1290,590 );await sleep(1000);
-    moveMouseTo(1440,330 );await sleep(1000);
-    moveMouseTo(1580,590 );await sleep(1000);
-    leftButtonUp();await sleep(500);
-    click(1440, 1025); await sleep(16000);
-    keyPress("ESCAPE"); await sleep(1000);
-    keyPress("ESCAPE"); await sleep(1000);
-
     await Purchase('璃月购买狗粮1');
     await Shopping();
 
@@ -78,21 +64,6 @@ let holdingState = settings.holdingState ?? 0;
     await Shopping();
 
     await Purchase('枫丹购买狗粮');
-    await sleep(1000);
-    keyDown("d");
-    await sleep(2000);
-    keyUp("d");
-    keyDown("a");
-    await sleep(300);
-    keyUp("a");
-    keyDown("w");
-    await sleep(6000);
-    keyUp("w");
-    keyDown("s");
-    await sleep(2000);
-    keyPress("SPACE");
-    await sleep(600);
-    keyUp("s");
     await Shopping();
 
     await Purchase('纳塔购买狗粮');
