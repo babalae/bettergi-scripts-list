5bd9b68dc20ff48344687abf65f03697c9c14fd9
ticktock <815773031@qq.com>
2025-10-12 22:59:47 +0800

    联机凑热闹:1.添加次数限制 2.优化联机状态判断逻辑

--- repo/js/联机凑热闹/main.js
+++ repo/js/联机凑热闹/main.js
@@ -6,6 +6,9 @@ const scrollTopPosition = Object.freeze({x: 2410,y: 270});
 // 滚动条底部坐标
 const scrollBottomPosition = Object.freeze({x: 2410,y: 1198});
 
+const onlineRo = RecognitionObject.TemplateMatch(file.ReadImageMatSync("assets/online.png"), 0, 0, genshin.width / 3.0, genshin.width / 5.0);
+const uiRo = RecognitionObject.TemplateMatch(file.ReadImageMatSync("assets/ui.png"), 0, 0, genshin.width / 3.0, genshin.width / 5.0);
+
 // 滚动联机列表并点击，最多点击8次到底部
 const scrollPageClick = async (count) => {
   if (count === null || count < 1){
@@ -46,27 +49,33 @@ const findRoom = async () => {
         let x = Math.floor(res.x * genshin.scaleTo1080PRatio);
         let y = Math.floor((res.y + 60) * genshin.scaleTo1080PRatio);
         click(x, y)
-        await sleep(12000)
-        isFindRoom = await isJoinRoom()
-        log.info("{x}",isFindRoom)
-        return;
+        log.info("等待15s");
+        await sleep(15000);
+
+        isFindRoom = await isJoinRoom(5000)
+        return true;
     }
   }
+  return false;
 }
 
 
-const isJoinRoom = async () => {
-    let captureRegion = captureGameRegion();
-    let resList = captureRegion.findMulti(RecognitionObject.ocrThis);
-    let result = true;
-    for (let i = 0; i < resList.count; i++) { // 遍历的是 C# 的 List 对象，所以要用 count，而不是 length
-      let res = resList[i];
-      if (res.text == "输入玩家UID搜索"){
-          result = false;
-          return result;
-      }
+const isJoinRoom = async (delay) => {
+  //按delay判断一次是否处于联机状态
+  for (let i = 0; i < 5; i++) {
+    // 最多 ESC 5次
+    let captureRegion = captureGameRegion();  // 获取一张截图
+    let result = captureRegion.Find(onlineRo);
+    if (result.isExist()) {
+      return true;
     }
-    return result;
+    result = captureRegion.Find(uiRo);
+    if (result.isExist()) {
+      return false;
+    } 
+    await sleep(delay);
+  }
+  return false;
 }
 
 const isLevelLimit = async () => {
@@ -88,10 +97,18 @@ const isLevelLimit = async () => {
 (async function () {
     
     setGameMetrics(2560, 1440, 1);
-    log.info("当前配置为：{join_type}",settings.join_type);
-    log.info("开始搜索房间");
+    log.info("当前配置为：");
+    log.info("方向设置：{join_type}",settings.join_type);
+    log.info("联机人数设置：{player_num}",settings.player_num);
+    log.info("总尝试次数：{repeat_num}",settings.repeat_num);
+    let current_num = 1;
     await genshin.returnMainUi()
-    while (!isFindRoom){
+    while (!isFindRoom && (settings.repeat_num === "0" ||current_num <= settings.repeat_num)){
+        if(await isJoinRoom(100)){
+          log.info("已处于联机状态，脚本结束");
+          break;
+        }
+        log.info("开始搜索房间，当前搜索次数{n}",current_num);
         keyPress("F2")
         await sleep(1000)
         if (settings.join_type === '往上翻'){
@@ -114,6 +131,7 @@ const isLevelLimit = async () => {
         }
         await genshin.returnMainUi()
         log.info("未找到房间，关掉联机重新寻找");
+        current_num = current_num + 1;
     }
     
     
