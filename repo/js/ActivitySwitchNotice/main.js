63fde43aa784265549cf87dddaff4bbd59449971
yan <doutianmianxia@qq.com>
2025-12-21 02:16:57 +0800

    feat(ActivitySwitchNotice): 增加返回主界面逻辑并优化设置提示
    - 新增 isInMainUI 函数用于判断当前是否处于主界面
    - 实现 toMainUi 异步函数确保脚本运行前回到主界面
    - 更新设置项标签，明确活动名称可选填且默认推送所有有剩余时间的活动
    - 调整代码结构以支持新增功能模块
    - 修复部分语法问题以提高代码健壮性

--- repo/js/ActivitySwitchNotice/main.js
+++ repo/js/ActivitySwitchNotice/main.js
@@ -1,11 +1,42 @@
-eval(file.readTextSync(`utils/activity.js`))
+eval(file.readTextSync(`utils/activity.js`));
+eval(file.readTextSync(`utils/notice.js`));
 
+// 判断是否在主界面的函数
+const isInMainUI = () => {
+    let captureRegion = captureGameRegion();
+    let res = captureRegion.Find(RecognitionObject.TemplateMatch(
+        file.ReadImageMatSync("assets/paimon_menu.png"),
+        0,
+        0,
+        640,
+        216
+    ));
+    captureRegion.dispose();
+    return !res.isEmpty();
+};
+
+async function toMainUi() {
+    let ms = 300
+    let index = 1
+    await wait();
+    while (!isInMainUI()) {
+        await sleep(ms);
+        await genshin.returnMainUi(); // 如果未启用，则返回游戏主界面
+        await sleep(ms);
+        if (index > 3) {
+            throw new Error(`多次尝试返回主界面失败`);
+        }
+        index += 1
+    }
+}
 (async function () {
+    await toMainUi();
     await main();
 })();
+
 /**
  * @returns {Promise<void>}
  */
-async function main(){
+async function main() {
     await activityUtil.activityMain()
 }
