470dd1c77155a790a6224ead3d74bfd0b2755fa3
h <1374375698@qq.com>
2026-03-27 13:43:15 +0800

    js：博士周本充能 (#3045)

--- repo/js/博士周本充能/main.js
+++ repo/js/博士周本充能/main.js
@@ -193,6 +193,8 @@ const RightSliderBottomRo = RecognitionObject.TemplateMatch(file.ReadImageMatSyn
 
         await keyMouseScript.runFile("222.json");
 
+		await sleep(1000);
+
         log.info("充能完成");
     }
 
@@ -322,6 +324,7 @@ const RightSliderBottomRo = RecognitionObject.TemplateMatch(file.ReadImageMatSyn
 			log.info("正在尝试切换至" + settings.partyName);
 			await SwitchParty(settings.partyName);
 			await goToWeeklyBossAndEnter();
+
 			await genshin.TpToStatueOfTheSeven();
 			genshin.clearPartyCache();
 
